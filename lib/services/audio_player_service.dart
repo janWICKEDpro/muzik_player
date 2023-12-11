75ef3b44d65bac0b22fc9ef1af12b608f8186e0f
janWICKEDpro <janroyal45@gmail.com>
2023-12-11 07:57:48 +0100

    permissions and audio player package

---
+++ lib/services/audio_player_service.dart
@@ -0,0 +1,15 @@
+import 'package:audioplayers/audioplayers.dart';
+
+class AudioPlayerService {
+  final _audioPlayer = AudioPlayer();
+
+  void pause() async {}
+
+  void play() async {}
+
+  void seek() async {}
+
+  void playNext() async {}
+
+  void playPrev() async {}
+}
