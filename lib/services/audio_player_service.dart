70100d047f364eab9429168a8b621bdd20b12852
janWICKEDpro <janroyal45@gmail.com>
2023-12-13 22:00:50 +0100

    songs provider in progress

--- lib/services/audio_player_service.dart
+++ lib/services/audio_player_service.dart
@@ -1,7 +1,18 @@
+import 'dart:io';
+
 import 'package:audioplayers/audioplayers.dart';
+import 'package:path_provider/path_provider.dart';
 
 class AudioPlayerService {
   final _audioPlayer = AudioPlayer();
+
+  Future<List<File>> getAudio() async {
+    List<File> audioFiles = [];
+
+    Directory? directory = await getExternalStorageDirectory();
+    if (directory != null) {}
+    return audioFiles;
+  }
 
   void pause() async {}
 
