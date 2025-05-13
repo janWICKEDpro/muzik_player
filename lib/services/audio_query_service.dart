c5192f1e26a3a7137402f704abb92a128142d771
janWICKEDpro <janroyal45@gmail.com>
2025-05-14 00:13:54 +0100

    resolve pertinent issues

--- lib/services/audio_query_service.dart
+++ lib/services/audio_query_service.dart
@@ -10,18 +10,26 @@ class AudioQueryService {
   static final AudioQueryService _instance = AudioQueryService._();
   factory AudioQueryService() => _instance;
 
-
   Future<bool> requestStoragePermission() async {
     if (Platform.isAndroid) {
       if (await isAndroidElevenOrLater()) {
-        if (!await Permission.audio.isGranted) {
-          final status = await Permission.audio.request();
-          return status.isGranted;
+        log("here 11 and up");
+        if (!(await Permission.manageExternalStorage.isGranted)) {
+          log("no manage external storage");
+          final status1 = await Permission.manageExternalStorage.request();
+          if (!(await Permission.audio.isGranted)) {
+            log("no audio permission");
+            final status = await Permission.audio.request();
+            return status.isGranted;
+          } else {
+            log("hmmm");
+            return true;
+          }
         } else {
           return true;
         }
       } else {
-        if (!await Permission.storage.isGranted) {
+        if (!(await Permission.storage.isGranted)) {
           final status = await Permission.storage.request();
           return status.isGranted;
         } else {
@@ -65,105 +73,104 @@ class AudioQueryService {
     Map<String, List<AudioMetadata>> artistMap = {};
     try {
       if (await downloadsDirectory.exists()) {
-        final entities = downloadsDirectory.listSync().toList();
+        final entities = await downloadsDirectory.list().toList();
         entities.removeWhere((e) => e.path == "/storage/emulated/0/Android");
-        for (var entity in entities) {
-          if (entity is Directory) {
-            entity.listSync(recursive: true).forEach((element) {
-              if (element is File && element.path.endsWith('.mp3')) {
+        await Future.forEach(entities, (entity) async {
+          try {
+            if (entity is Directory) {
+              final subEntities = await entity.list(recursive: true).toList();
+              await Future.forEach(subEntities, (element) async {
+                if (element is File && element.path.endsWith('.mp3')) {
+                  try {
+                    final metadata =
+                        await readMetadata(File(element.path), getImage: true);
+                    log('$metadata');
+                    if (metadata.album != null) {
+                      albumKeys.update(
+                        metadata.album!,
+                        (val) => val..add(metadata),
+                        ifAbsent: () => [metadata],
+                      );
+                    } else {
+                      albumKeys.update(
+                        "Unknown Album",
+                        (val) => val..add(metadata),
+                        ifAbsent: () => [metadata],
+                      );
+                    }
+                    if (metadata.artist != null) {
+                      artistMap.update(
+                        metadata.artist!,
+                        (val) => val..add(metadata),
+                        ifAbsent: () => [metadata],
+                      );
+                    }
+                    foundMp3s.add(metadata);
+                  } catch (e) {
+                    log('Error reading metadata: $e');
+                  }
+                }
+              });
+            }
+            if (entity is File && entity.path.endsWith('.mp3')) {
+              try {
                 final metadata =
-                    readMetadata(File(element.path), getImage: true);
-                log('$metadata');
+                    await readMetadata(File(entity.path), getImage: true);
                 if (metadata.album != null) {
-                  log('Got here');
                   albumKeys.update(
                     metadata.album!,
-                    (val) {
-                      final newList = val..add(metadata);
-                      return newList;
-                    },
-                    ifAbsent: () => [metadata],
-                  );
-                }else {
-                   albumKeys.update(
-                    "Unkown Album",
-                    (val) {
-                      final newList = val..add(metadata);
-                      return newList;
-                    },
+                    (val) => val..add(metadata),
                     ifAbsent: () => [metadata],
                   );
                 }
                 if (metadata.artist != null) {
                   artistMap.update(
                     metadata.artist!,
-                    (val) {
-                      final newList = val..add(metadata);
-                      return newList;
-                    },
+                    (val) => val..add(metadata),
                     ifAbsent: () => [metadata],
                   );
                 }
                 foundMp3s.add(metadata);
+              } catch (e) {
+                log('Error reading metadata: $e');
               }
-            });
-          }
-          if (entity is File) {
-            final metadata = readMetadata(File(entity.path), getImage: true);
-            if (metadata.album != null) {
-              albumKeys.update(
-                metadata.album!,
-                (val) {
-                  final newList = val..add(metadata);
-                  return newList;
-                },
-                ifAbsent: () => [metadata],
-              );
             }
-            if (metadata.artist != null) {
-              artistMap.update(
-                metadata.artist!,
-                (val) {
-                  final newList = val..add(metadata);
-                  return newList;
-                },
-                ifAbsent: () => [metadata],
-              );
-            }
-            foundMp3s.add(metadata);
+          } catch (e) {
+            log('Error accessing entity: ${entity.path}, $e');
           }
-        }
-        for (var key in albumKeys.keys) {
+        });
+        albumKeys.forEach((key, value) {
           albums.add(
             Album(
-                name: key,
-                author: albumKeys[key]?.first.artist ?? "Unkown",
-                songs: albumKeys[key] ?? []),
+              name: key,
+              author: value.first.artist ?? "Unknown",
+              songs: value,
+            ),
           );
-        }
-        for (var key in artistMap.keys) {
+        });
+        artistMap.forEach((key, value) {
           artists.add(
             Artist(
-                name: key,
-                songs: albumKeys[key]?.length ?? 0,
-                albums: getAlbumCount(key, foundMp3s),
-                audios: albumKeys[key] ?? []),
+              name: key,
+              songs: value.length,
+              albums: getAlbumCount(key, foundMp3s),
+              audios: value,
+            ),
           );
-        }
+        });
         return (foundMp3s, albums, artists);
       } else {
         const statusMessage =
             "Downloads directory not found at $downloadsDirectoryPath";
         log(statusMessage);
-        throw Exception("An Error Occured");
+        throw Exception("An Error Occurred");
       }
     } catch (e) {
       final statusMessage = "Error reading directory: $e";
       log(statusMessage);
-      throw Exception("Error Occured while fetching audio");
+      throw Exception("Error Occurred while fetching audio");
     }
   }
-
 
   int getAlbumCount(String artist, List<AudioMetadata> audios) {
     int count = 0;
@@ -174,7 +181,6 @@ class AudioQueryService {
     }
     return count;
   }
-
 }
 
 class Album {
